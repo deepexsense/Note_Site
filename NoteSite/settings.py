3e75a97abf978a3b7862f575abbd3ab4a35207b2
deepexsense <dashka2h@yandex.ru>
2019-04-15 14:40:52 +0300

    New settings

--- NoteSite/settings.py
+++ NoteSite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'deepnotesite.herokuapp.com',
+    '127.0.0.1',
 ]
 
 
@@ -77,10 +78,15 @@ WSGI_APPLICATION = 'NoteSite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
     }
 }
 
