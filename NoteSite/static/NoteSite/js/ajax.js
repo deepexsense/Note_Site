fe7aefc1e1ad66fc2a206e392d28c421ab9f93bf
deepexsense <dashka2h@yandex.ru>
2019-04-26 14:01:18 +0300

    Update ajax.js

--- NoteSite/static/NoteSite/js/ajax.js
+++ NoteSite/static/NoteSite/js/ajax.js
@@ -1,25 +1,4 @@
 $(document).ready(function () {
-    // $(document).on("submit", "#id_register_form", function (e) {
-    //     e.preventDefault();
-    //
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "/register/",
-    //         data: {
-    //             username: $('#id_username').val(),
-    //             password1: $('#id_password1').val(),
-    //             password2: $('#id_password2').val(),
-    //             csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
-    //         },
-    //         dataType: "html",
-    //         success: function () {
-    //             alert("Created new account! Now you can sign in!")
-    //         },
-    //         error: function () {
-    //             alert("Please input correct username and password!")
-    //         }
-    //     });
-    // });
 
     $(document).on("click", "#id_sign_up", function (e) {
         e.preventDefault();
